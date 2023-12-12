dcc7f3ee584f6e4a3bdaedd88315cce92b4c75d2
Dimarik <dimarik2003@gmail.com>
2023-12-12 12:39:20 +0200

    Добавил подсчет строк, создание нового массиваб заполнение нового массива строк и вывод массива строк
    Signed-off-by: Dimarik <dimarik2003@gmail.com>

--- Program.cs
+++ Program.cs
@@ -17,3 +17,40 @@ string[] resultArray = FilterArray(inputArray);
 // Вывод результата
 Console.WriteLine("Результат:");
 PrintArray(resultArray);
+
+string[] FilterArray(string[] inputArray)
+{
+    int count = 0;
+
+    // Подсчет строк, удовлетворяющих условию
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Создание нового массива строк
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    // Заполнение нового массива строк
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+    return resultArray;
+}
+void PrintArray(string[] array)
+{
+    // Вывод массива строк
+    foreach (string element in array)
+    {
+        Console.WriteLine(element);
+    }
+}
